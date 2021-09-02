541ca16d3fed7ade4f04d12ef2ef1f92c4f906e5
Yulei Liu <yulei.liu@gmail.com>
2021-09-02 15:32:59 +1000

    add iam:ListAttachedUserPolicies

--- main.tf
+++ main.tf
@@ -114,6 +114,7 @@ data "aws_iam_policy_document" "this" {
       "iam:CreateAccessKey",
       "iam:ListGroupsForUser",
       "iam:ListUserPolicies",
+      "iam:ListAttachedUserPolicies",
       "iam:GetUser",
       "iam:GetRole",
       "autoscaling:DescribeAutoScalingGroups",
