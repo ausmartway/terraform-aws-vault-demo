7c48b3becfd62c7dd056f865dba1d94a65cc7fb9
Yulei Liu <yulei.liu@gmail.com>
2023-08-25 16:57:31 +1000

    syntex

---
+++ APA.tf
@@ -0,0 +1,26 @@
+module "apa-vault" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.3.1"
+
+  name           = "apa-vault"
+
+  private_ip = "10.0.101.162"
+
+  user_data_base64 = base64gzip(data.template_file.userdata.rendered)
+
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = var.instance_type
+  key_name             = var.key_name
+  iam_instance_profile = aws_iam_instance_profile.this.name
+  associate_public_ip_address = true
+
+  monitoring = true
+  vpc_security_group_ids = [
+    local.security_group_outbound,
+    local.security_group_ssh,
+    module.security_group_vault.security_group_id
+  ]
+
+  subnet_id = local.public_subnets[0]
+  tags      = var.tags
+}
