8766da265c1de2facb0cbcc870c79bd35e095793
Yulei Liu <yulei.liu@gmail.com>
2023-11-10 10:23:02 +1100

    decommission

--- config.tf
+++ config.tf
@@ -1,16 +1,16 @@
-locals {
-  user_data = templatefile("${path.module}/templates/userdata.yaml", {
-    ip_address    = var.private_ip,
-    vault_license = var.vault_license
-    vault_conf = base64encode(templatefile("${path.module}/templates/vault.conf",
-      {
-        listener     = var.private_ip
-        ip_addresses = [var.private_ip]
-        node_id      = var.hostname
-        leader_ip    = var.private_ip
-        kms_key_id   = aws_kms_key.aws_kms_key.id
-      }
-    ))
-    slack_webhook = var.slack_webhook
-  })
-}
+# locals {
+#   user_data = templatefile("${path.module}/templates/userdata.yaml", {
+#     ip_address    = var.private_ip,
+#     vault_license = var.vault_license
+#     vault_conf = base64encode(templatefile("${path.module}/templates/vault.conf",
+#       {
+#         listener     = var.private_ip
+#         ip_addresses = [var.private_ip]
+#         node_id      = var.hostname
+#         leader_ip    = var.private_ip
+#         kms_key_id   = aws_kms_key.aws_kms_key.id
+#       }
+#     ))
+#     slack_webhook = var.slack_webhook
+#   })
+# }
